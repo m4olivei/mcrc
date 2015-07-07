146145596631b055bd8cb54576bca0ea406e3dd7
Matthew Oliveira <m4olivei@gmail.com>
2015-07-06 21:40:41 -0400

    Add a flag that ignores file times for drush rsync command.

--- drupal/sites/default/drushrc.php
+++ drupal/sites/default/drushrc.php
@@ -3,4 +3,4 @@
  * Useful shell aliases:
  */
 $options['shell-aliases']['pull-live'] = '!drush sql-sync @mcrc.live @mcrc.dev --create-db --no-cache --structure-tables-key=common --sanitize --sanitize-email=user+%uid@localhost -y';
-$options['shell-aliases']['pull-live-files'] = '!drush rsync -v @mcrc.live:%files @mcrc.dev:%files';
+$options['shell-aliases']['pull-live-files'] = '!drush rsync -v --omit-dir-times @mcrc.live:%files @mcrc.dev:%files';
