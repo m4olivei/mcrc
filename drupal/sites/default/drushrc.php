5a5176b482c48cd69bc2c71b6ddc4372407b91ad
Matthew Oliveira <m4olivei@gmail.com>
2016-02-15 14:10:52 -0500

    Remove --no-cache option which is now obselete.

--- drupal/sites/default/drushrc.php
+++ drupal/sites/default/drushrc.php
@@ -2,5 +2,5 @@
 /**
  * Useful shell aliases:
  */
-$options['shell-aliases']['pull-live'] = '!drush sql-sync @mcrc.live @mcrc.dev --create-db --no-cache --structure-tables-key=common --sanitize --sanitize-email=user+%uid@localhost -y';
+$options['shell-aliases']['pull-live'] = '!drush sql-sync @mcrc.live @mcrc.dev --create-db --structure-tables-key=common --sanitize --sanitize-email=user+%uid@localhost -y';
 $options['shell-aliases']['pull-live-files'] = '!drush rsync -v --omit-dir-times @mcrc.live:%files @mcrc.dev:%files';
