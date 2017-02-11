7622067128772257257f8a5db94f87dd4053b857
Matthew Oliveira <m4olivei@gmail.com>
2017-02-11 16:52:17 -0500

    Update Drupal core to 7.54.

--- drupal/sites/default/drushrc.php
+++ drupal/sites/default/drushrc.php
@@ -4,3 +4,8 @@
  */
 $options['shell-aliases']['pull-live'] = '!drush sql-sync @mcrc.live @mcrc.dev --create-db --structure-tables-key=common --sanitize --sanitize-email=user+%uid@localhost -y';
 $options['shell-aliases']['pull-live-files'] = '!drush rsync -v --omit-dir-times @mcrc.live:%files @mcrc.dev:%files';
+
+/**
+ * Drush patch file location.
+ */
+$options['patch-file'] = '../patches.make';
